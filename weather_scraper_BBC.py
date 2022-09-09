66dcfd5c7acf8b4ec52587bed74e34517e9f3367
ShagunDwivedi <711shagun@gmail.com>
2022-09-09 23:27:00 +0530

    Final Commit

--- weather_scraper_BBC.py
+++ weather_scraper_BBC.py
@@ -4,12 +4,12 @@ from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
 
-import pandas
+import pandas as pd
 import re
 
 from datetime import datetime
 
-required_city = "Mumbai"
+required_city = input("Enter City Name: ")
 location_url = 'https://locator-service.api.bbci.co.uk/locations?' + urlencode({
    'api_key': 'AGbFAKx58hyjQScCXIYrxuEwJh2W2cmv',
    's': required_city,
@@ -38,3 +38,32 @@ low_temp_daily = soup.find_all('span', attrs={'class': 'wr-day-temperature__low-
 #daily summary
 summ_daily = soup.find('div', attrs={'class': 'wr-day-summary'})
 
+high_temp_daily_list = [high_temp_daily[i].text.strip().split()[0] for i in range(len(high_temp_daily))]
+low_temp_daily_list = [low_temp_daily[i].text.strip().split()[0] for i in range(len(low_temp_daily))]
+summ_daily_list = re.findall('[a-zA-Z][^A-Z]*', summ_daily.text)
+
+#for making a pandas dataframe
+datelist = pd.date_range(datetime.today(), periods=len(high_temp_daily)).tolist()
+
+datelist = [datelist[i].date().strftime('%y-%m-%d') for i in range(len(datelist))]
+zipping = zip(datelist, high_temp_daily, low_temp_daily, summ_daily)
+
+
+df = pd.DataFrame(list(zipping), columns=['Date', 'Highest', 'Lowest', 'Summary'])
+#print(df.describe())
+
+loc = soup.find('h1', attrs={'id': 'wr-location-name-id'})
+
+#to create a csv_file
+#csv_filename = loc.text.split()[0]+'.csv'
+#df.to_csv(csv_filename, index=None)
+
+#to create xlsx_file
+#xlsx_filename = loc.text.split()[0]+".xlsx"
+#df.to_excel(xlsx_filename)
+
+print(required_city, " Today")
+print("Highest Temperature: ",high_temp_daily_list[0])
+print("Lowest Temperature: ", low_temp_daily_list[0])
+print("Summary: ", summ_daily_list[0])
+#print(loc.text.split("- ")[1])
